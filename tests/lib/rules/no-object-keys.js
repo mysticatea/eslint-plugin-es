d76c1514f2cac93cf2421264d21c9e6a7fb95cca
yaroslavnikiforov <yaroslav.nikiforov.93@gmail.com>
2019-10-20 18:51:28 +0300

    fix CI / Lint checks failing

--- tests/lib/rules/no-object-keys.js
+++ tests/lib/rules/no-object-keys.js
@@ -2,17 +2,17 @@
  * @author Toru Nagashima <https://github.com/mysticatea>
  * See LICENSE file in root directory for full license.
  */
-"use strict";
+"use strict"
 
-const RuleTester = require("../../tester");
-const rule = require("../../../lib/rules/no-object-keys.js");
+const RuleTester = require("../../tester")
+const rule = require("../../../lib/rules/no-object-keys.js")
 
 new RuleTester().run("no-object-keys", rule, {
-  valid: ["Object", "Object.assign", "let Object = 0; Object.keys"],
-  invalid: [
-    {
-      code: "Object.keys",
-      errors: ["ES5 'Object.keys' method is forbidden."]
-    }
-  ]
-});
+    valid: ["Object", "Object.assign", "let Object = 0; Object.keys"],
+    invalid: [
+        {
+            code: "Object.keys",
+            errors: ["ES5 'Object.keys' method is forbidden."],
+        },
+    ],
+})
